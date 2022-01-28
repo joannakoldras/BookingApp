d9275c5d63724bb724d9d7fe4d4fed1daa37871e
joannakoldras <joanna.koldras@microsoft.wsei.edu.pl>
2022-01-28 19:13:04 +0100

    Create BookingContext.cs

---
+++ BookingApp/BookingApp/Context/BookingContext.cs
@@ -0,0 +1,31 @@
+﻿using BookingApp.Data;
+using BookingApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingApp.Context
+{
+    public class BookingContext : DbContext
+    {
+        public BookingContext(DbContextOptions<BookingContext> options)
+              : base(options)
+        {
+        }
+   
+        public DbSet<Client> Clients { get; set; }
+
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+
+   
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+          
+        }
+    }
+}
