ece4d435743573f3ea328c30716562faebeacb4b
joannakoldras <joanna.koldras@microsoft.wsei.edu.pl>
2022-01-28 19:13:26 +0100

    Create Client.cs

---
+++ BookingApp/BookingApp/Models/Client.cs
@@ -0,0 +1,22 @@
+﻿using BookingApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingApp.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string IdentityCardNumber { get; set; }
+        public string  PhoneNumber { get; set; }
+        public string Number { get; set; }//email
+
+        public virtual ICollection<Reservation> Reservations { get; set; }
+       
+       
+    }
+}
