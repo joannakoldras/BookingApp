5b40725f9aceef638c1a3f0d5978065ed442345c
joannakoldras <joanna.koldras@microsoft.wsei.edu.pl>
2022-01-28 19:13:31 +0100

    Create Room.cs

---
+++ BookingApp/BookingApp/Models/Room.cs
@@ -0,0 +1,20 @@
+﻿using BookingApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingApp.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public int Floor { get; set; }
+        public int RoomNumber { get; set; }
+        public int NumberOfDoubleBeds { get; set; }
+        public int NumberOfSingleBeds { get; set; }
+        public int NumbefOfPeople{ get; set; }
+        public decimal Price { get; set; }
+        public virtual ICollection<Reservation> Reservations { get; set; }
+    }
+}
