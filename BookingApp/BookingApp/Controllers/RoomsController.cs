7892e7bfa175146dc62d866d225aae00e0982a27
joannakoldras <joanna.koldras@microsoft.wsei.edu.pl>
2022-01-28 19:13:12 +0100

    Create RoomsController.cs

---
+++ BookingApp/BookingApp/Controllers/RoomsController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BookingApp.Context;
+using BookingApp.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace BookingApp.Controllers
+{
+    [Authorize]
+    public class RoomsController : Controller
+    {
+        private readonly BookingContext _context;
+
+        public RoomsController(BookingContext context)
+        {
+            _context = context;
+        }
+
+      
+
+        public async Task<IActionResult> FreeRooms()
+        {
+    
+            return View(await GetRoomsFound(DateTime.Now, DateTime.Now.AddDays(10)));
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Rooms.ToListAsync());
+        }
+
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var room = await _context.Rooms
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+
+            return View(room);
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+  
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Floor,RoomNumber,NumberOfDoubleBeds,NumberOfSingleBeds,NumbefOfPeople,Price")] Room room)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(room);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(room);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var room = await _context.Rooms.FindAsync(id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+            return View(room);
+        }
+
+    
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Floor,RoomNumber,NumberOfDoubleBeds,NumberOfSingleBeds,NumbefOfPeople,Price")] Room room)
+        {
+            if (id != room.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(room);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RoomExists(room.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(room);
+        }
+
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var room = await _context.Rooms
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+
+            return View(room);
+        }
+
+    
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var room = await _context.Rooms.FindAsync(id);
+            _context.Rooms.Remove(room);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RoomExists(int id)
+        {
+            return _context.Rooms.Any(e => e.Id == id);
+        }
+
+        public async Task<List<Room>> GetRoomsFound(DateTime? dateFrom, DateTime? dateTo)
+        {
+
+            var sel = from room in _context.Rooms
+                     where !_context.Reservations.Any(reservation => reservation.Room.Id == room.Id
+                     && reservation.DateFrom == dateFrom && reservation.DateTo == dateTo)
+            select room;
+            return await sel.ToListAsync();
+        }
+
+
+    }
+}
